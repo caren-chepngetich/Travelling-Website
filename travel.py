123619a424eb819ef44011ddf1b2caecccbcce85
Student <Caren-Chepngetich>
2024-03-30 00:12:18 +0300

    made changes

--- travel.py
+++ travel.py
@@ -1,17 +1,14 @@
-from tkinter import*
-root = Tk()
-root . geometry("500*300")
 
-Label(root, text = "Python Registration form",font ="ar 15 bold").grid(row=0,column=)
-name = label(root, text="Name")
-phone = label(root, text="Phone")
-gender = label(root, text="Gender")
-emergency = label(root, text="Emergency")
-mode of payment = label(root, text="mode of payment")
-name.grid(row=1,colum=2)
-phone.grid(row=2,colum=2)
-gender.grid(row=3,colum=2)
-emergency.grid(row=4,colum=2)
-mode of payment.grid(row=5,colum=2)
-
-root.mainloop()
+import random
+n = random.randrange(3, 5)
+guess = int(input("Enter any number: "))
+while n != guess:
+    if guess < n :
+        print("Too low")
+        guess = int(input("Enter number again: "))
+    elif guess > n:
+            print("Too high!")
+            guess = int(input("Enter number again"))
+    else:
+        break
+print("you guessed it right")
