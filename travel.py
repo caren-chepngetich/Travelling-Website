b5d408599a3113f897f8813203ab107d851321ce
Student <Caren-Chepngetich>
2024-03-30 23:35:50 +0300

    made changes

--- travel.py
+++ travel.py
@@ -12,3 +12,18 @@ while n != guess:
     else:
         break
 print("you guessed it right")
+
+
+#Average(Mean)
+list1 = [1509,6776,6565,2343,2524,2524,2656,25256]
+mean = sum(list1)/len(list1)
+print (mean)
+
+
+#Collage of photos
+def collage_maker(image1, image2, name);
+     i1 =np.array(image1)
+     i2 =np.array(image2)
+     collage =np.vstack([i1, i2])
+     image = image.fromarray(collage)
+     image.save(name)
