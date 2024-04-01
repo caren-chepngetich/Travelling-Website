630389433b6631bcd54682cbcd741ef18c431a7a
Student <Caren-Chepngetich>
2024-04-01 21:39:26 +0300

    made changes

--- travel.py
+++ travel.py
@@ -1,6 +1,6 @@
 
 import random
-n = random.randrange(3, 5)
+n = random.randrange(5000, 10000)
 guess = int(input("Enter any number: "))
 while n != guess:
     if guess < n :
@@ -10,7 +10,7 @@ while n != guess:
             print("Too high!")
             guess = int(input("Enter number again"))
     else:
-        break
+    break
 print("you guessed it right")
 
 
@@ -20,10 +20,4 @@ mean = sum(list1)/len(list1)
 print (mean)
 
 
-#Collage of photos
-def collage_maker(image1, image2, name);
-     i1 =np.array(image1)
-     i2 =np.array(image2)
-     collage =np.vstack([i1, i2])
-     image = image.fromarray(collage)
-     image.save(name)
+
